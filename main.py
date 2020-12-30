0c883bbcbaa139a7095d9b0a1e913f460d8d7aa9
Petr Ancinec <ancinpet@fit.cvut.cz>
2020-12-30 02:30:01 +0100

    Added AOB based code detection example + new exe

--- main.py
+++ main.py
@@ -1,59 +1,59 @@
 import sys
+import random
+import time
 sys.path.insert(1, 'WMemPy')
+sys.path.insert(1, '../../WMemPy')
 from wmem_process import WinProc
-from ctypes import wintypes
-import ctypes
+
+# Bad code:
+# 0x83ec8b55,0x565340ec,0x0c758b57,0x8b087d8b,
+# 0x348d104d,0xcf3c8dce,0x6f0fd9f7,0x6f0fce04,
+# 0x0f08ce4c,0x10ce546f,0xce5c6f0f,0x646f0f18,
+# 0x6f0f20ce,0x0f28ce6c,0x30ce746f,0xce7c6f0f,
+# 0x04e70f38,0x4ce70fcf,0xe70f08cf,0x0f10cf54,
+# 0x18cf5ce7,0xcf64e70f,0x6ce70f20,0xe70f28cf,
+# 0x0f30cf74,0x38cf7ce7,0x7508c183,0xf8ae0fad,
+# 0x5e5f770f,0x5de58b5b,0xccccccc3
+
+potential_threats = ['WMemPy_test_app.exe', 'WMemPy_test_app_poly.exe', 'csrss.exe', 'dwm.exe', 'WMemPY_hello_app.exe']
+bad_code_patterns = ['55 8b ec 83 ec 40 53 56 57 8b 75 0c 8b 7d 08 8b',
+                     '4d 10 8d 34 ce 8d 3c cf f7 d9 0f 6f 04 ce 0f 6f',
+                     '4c ce 08 0f 6f 54 ce 10 0f 6f 5c ce 18 0f 6f 64',
+                     'ce 20 0f 6f 6c ce 28 0f 6f 74 ce 30 0f 6f 7c ce',
+                     '38 0f e7 04 cf 0f e7 4c cf 08 0f e7 54 cf 10 0f',
+                     'e7 5c cf 18 0f e7 64 cf 20 0f e7 6c cf 28 0f e7',
+                     '74 cf 30 0f e7 7c cf 38 83 c1 08 75 ad 0f ae f8']
 
 
-def get_team_string(team_num):
-    if team_num == 1:
-        return 'Spectator'
-    if team_num == 2:
-        return 'Terrorist'
-    if team_num == 3:
-        return 'Counter-Terrorist'
-    return 'Unassigned'
+while True:
+    print('Starting check for bad code.')
+    for app in potential_threats:
+        print(f'App: {app}')
+        matches = []
+        try:
+            proc = WinProc(app)
+        except Exception:
+            continue
+        main_entry = [module for module in proc.modules if module.get_name() == app]
+        random.shuffle(bad_code_patterns)
+        for code in bad_code_patterns:
+            result = proc.scanner.AOB_scan_arr(main_entry, code)
+            if not (result[0] is None):
+                matches.append([result, code])
+        if len(matches) >= 4:
+            print(f'Detected application with bad code: {proc.proc_name} ({len(matches)}/{len(bad_code_patterns)} matches)')
+            print('Matches:')
+            print('-------------------')
+            for match in matches:
+                print(f'Address: {hex(match[0][1].base_address + match[0][0])}, code: {match[1]}')
+            print('-------------------')
+        elif len(matches) >= 1:
+            print(f'Application {proc.proc_name} is suspicious, but only has a few matches ({len(matches)}/{len(bad_code_patterns)} matches)')
+            print('Matches:')
+            print('-------------------')
+            for match in matches:
+                print(f'Address: {hex(match[0][1].base_address + match[0][0])}, code: {match[1]}')
+            print('-------------------')
 
-def get_opposite_team(team_num):
-    if team_num == 2:
-        return 3
-    if team_num == 3:
-        return 2
-    return team_num
-
-app_name = 'csgo.exe'
-csgo = WinProc(app_name)
-# Check what the app loads
-csgo.print_modules()
-# Get client.dll
-client = [module for module in csgo.modules if module.get_name() == 'client.dll'][0]
-# Try scanning for local player pointer in CSGO memory (we know it is in client.dll so we can speed up the scan)
-local_player_start = csgo.scanner.AOB_scan(client, "8D 34 85 ? ? ? ? 89 15 ? ? ? ? 8B 41 08 8B 48 04 83 F9 FF") + 3
-# We found the pointer in memory, read it so we can save it
-local_player_offset_absolute = client.read_dtype(local_player_start, wintypes.DWORD()) + 4
-# We want address relative to client.dll, not to whole csgo.exe process
-local_player_offset = local_player_offset_absolute - client.base_address
-print(f'The pointer to local player is located in {client.get_name()} at address {hex(local_player_offset)}')
-# We have address of pointer to local player, reading it lands us with pointer to local player aka
-# address, where local_player really is at in csgo memory, this changes each time you reload map / die etc...
-local_player_pointer = client.read_dtype(local_player_offset, wintypes.DWORD())
-# If you reverse engineer the player structure of CSGO, we can find some cool values, such as team, health etc...
-# Offsets can be found at https://github.com/frk1/hazedumper/blob/master/csgo.json
-my_team = csgo.reader.dtype(local_player_pointer + 0xF4, ctypes.c_int())
-my_health = csgo.reader.dtype(local_player_pointer + 0x100, ctypes.c_int())
-print(f'My health is {my_health} and my team is {get_team_string(my_team)}')
-
-print('Reversing the team number will make the game think we are on the enemy team and it will allow us to see enemies as if they were allies :)')
-enemy_team_num = get_opposite_team(my_team)
-# Due to how the Source engine works, we need to force our memory into the process
-# That means keep writing it, until a call in the Source engine happens where the teams get updated
-# After that, the new value will stay permanently until end of the game or next half
-# The update happens during gametick, we just need to time into it (bruteforce)
-for i in range(5000):
-    csgo.writer.dtype(local_player_pointer + 0xF4, ctypes.c_int(enemy_team_num))
-
-my_team = csgo.reader.dtype(local_player_pointer + 0xF4, ctypes.c_int())
-my_health = csgo.reader.dtype(local_player_pointer + 0x100, ctypes.c_int())
-print(f'My health is {my_health} and my team is {get_team_string(my_team)}')
-
-# Result: https://imgur.com/a/vN1X3gL
+    print('')
+    time.sleep(20)
