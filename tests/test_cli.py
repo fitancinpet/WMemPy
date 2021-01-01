20d5a4045390996fa1a1fa18674f4f669fdec034
Petr Ancinec <ancinpet@fit.cvut.cz>
2021-01-01 18:01:46 +0100

    cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,6 +2,8 @@ import pytest
 from flexmock import flexmock
 from wmempy.wmem_cli import *
 # Tests not covered here are covered by live memory tests
+# Assumes Windows 10 20H2 or lower (process System running at PID 4, System Idle Processes running at PID 0)
+# Most likely won't change in upcoming versions
 
 
 def test_array_from_where():
@@ -61,3 +63,41 @@ def test_memory_view_print(capsys):
     assert '0x10' in captured
     assert '[62 63 64 65 46 47 48 49 62 63 64 65 46 47 48 49]' in captured
     assert 'bcdeFGHIbcdeFGHI' in captured
+
+def test_get_proc():
+    """Test that get_proc fails on wrong values"""
+    with pytest.raises(Exception):
+        get_proc('System')
+    with pytest.raises(Exception):
+        get_proc('4')
+    with pytest.raises(Exception):
+        get_proc('0')
+    with pytest.raises(Exception):
+        get_proc('-1')
+
+def test_compare_procs():
+    """Test that compare_procs fails on wrong values"""
+    with pytest.raises(Exception):
+        compare_procs('System', '4')
+    with pytest.raises(Exception):
+        compare_procs('System', 'dwm.exe')
+    with pytest.raises(Exception):
+        compare_procs('dwm.exe', '4')
+    with pytest.raises(Exception):
+        compare_procs('0', '0')
+    with pytest.raises(Exception):
+        compare_procs('-1', 'dwm.exe')
+
+
+def test_run_app():
+    """Test that run_app stops with bad parameters"""
+    with pytest.raises(Exception):
+        run_app(None, None, None, None, None, None, None, None, None, None, None)
+    with pytest.raises(Exception):
+        run_app('System', None, None, None, None, None, None, None, None, None, None)
+    with pytest.raises(Exception):
+        run_app('dwm.exe', 4, None, None, None, None, None, None, None, None, None)
+    with pytest.raises(Exception):
+        run_app('System', -1, None, None, None, None, None, None, None, None, None)
+    with pytest.raises(Exception):
+        run_app(None, -1, None, None, None, None, None, None, None, None, None)
